6f52826c84551459423fba17c697136078c5555c
Thaysa Rafaele Gomes Lacerda <thaysa.lacerda@byseven.com.br>
2023-01-28 15:45:14 -0400

    implementando o remove transação

--- script.js
+++ script.js
@@ -77,12 +77,13 @@ const DOM = {
 
     addTransaction(transaction, index){
         const tr = document.createElement('tr');
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+        tr.innerHTML.index = index;
 
         DOM.transactionsContainer.appendChild(tr);
     },
 
-    innerHTMLTransaction(transaction){
+    innerHTMLTransaction(transaction, index){
         const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
         const amount = Utils.formatCurrency(transaction.amount);
@@ -92,7 +93,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/assets/minus.svg" alt="remover Transação">
+                <img onclick="Transaction.remove(${index})" src="./assets/assets/minus.svg" alt="remover Transação">
             </td>
         `
         return html;
@@ -214,9 +215,7 @@ const Form = {
 
 const App = {
     init(){
-        Transaction.all.forEach(transaction => {
-                DOM.addTransaction(transaction);
-        });
+        Transaction.all.forEach(DOM.addTransaction);
 
         DOM.updateBalance();
     },
